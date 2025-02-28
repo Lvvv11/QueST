7b8d2f47c79275ae6705b7edac2319478a7e1e6e
Lvvv11 <145366903+Lvvv11@users.noreply.github.com>
2025-02-28 21:03:54 +0900

    Update fed_avg.py

--- utils/fed_avg.py
+++ utils/fed_avg.py
@@ -5,7 +5,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.utils.data import DataLoader
-from fed_utils.sampler import FederatedSampler
+from utils.sampler import FederatedSampler
 from hydra.utils import instantiate
 import quest.utils.utils as utils
 from tqdm import tqdm
